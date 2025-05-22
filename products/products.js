800b3e2e9e7254f58c5780802858b9920779532e
Alex Wang <alex.yp.wang@gmail.com>
2025-05-22 13:39:47 -0500

    first iteration complete

--- products/products.js
+++ products/products.js
@@ -1,17 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const dropdown = document.querySelector(".dropdown");
-    const dropdownMenu = document.querySelector(".dropdown-menu");
-
-    dropdown.addEventListener("mouseenter", () => {
-        dropdownMenu.style.opacity = "1";
-        dropdownMenu.style.visibility = "visible"
-    });
-
-    dropdown.addEventListener("mouseleave", () => {
-        dropdownMenu.style.opacity = "0";
-        dropdownMenu.style.visibility = "hidden"
-    });
-
     const previewContainers = document.querySelectorAll(".product-preview");
     const mainImage = document.getElementById("main-img");
 
